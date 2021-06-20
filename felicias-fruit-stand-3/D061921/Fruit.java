2ea93ff67e2c6e91891be34e5f418c7ffd554381
Freddy Canido <54070099+Fred555-REV@users.noreply.github.com>
2021-06-20 01:16:17 -0400

    Update Fruit.java

--- felicias-fruit-stand-3/D061921/Fruit.java
+++ felicias-fruit-stand-3/D061921/Fruit.java
@@ -15,9 +15,9 @@ public class Fruit extends Product {
 
     public String toString() {
         if (amount > 1 || amount == 0) {
-            return "There are " + amount + " " + size + " " + color + " " + name + "s that weigh a total of " + weight + " pounds, they each cost $" + price + " each.";
+            return "There are " + amount + " " + size + " " + color + " " + name + "s that weigh a total of " + weight + " pounds, they each cost $" + price + ".";
         } else if (amount == 1) {
-            return "There is " + amount + " " + size + " " + color + " "+ name + " that weighs " + weight + " pounds, it costs $" + price;
+            return "There is " + amount + " " + size + " " + color + " "+ name + " that weighs " + weight + " pounds, it costs $" + price + ".";
         }
         return "ERR negative amount";
     }
