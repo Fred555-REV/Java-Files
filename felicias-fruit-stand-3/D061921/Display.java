35ddbcd48a5281445a0c3956d5fd9fa6076ff853
Freddy Canido <54070099+Fred555-REV@users.noreply.github.com>
2021-06-21 19:27:19 -0400

    Update Display.java

--- felicias-fruit-stand-3/D061921/Display.java
+++ felicias-fruit-stand-3/D061921/Display.java
@@ -1,17 +1,20 @@
 package D061921;
 
 import java.util.List;
+import java.util.Locale;
 
 public class Display {
     public static void displayProducts(List<Product> productList, String type, int fruitA, int meatA) {
 
-        switch (type) {
+        switch (type.toLowerCase(Locale.ROOT)) {
             case "meat":
+            case "m":
                 for (int i = 0; i < productList.size() - fruitA; i++) {
                     System.out.println(productList.get(i));
                 }
                 break;
             case "fruit":
+            case "f":
                 for (int i = meatA; i < productList.size(); i++) {
                     System.out.println(productList.get(i));
                 }
