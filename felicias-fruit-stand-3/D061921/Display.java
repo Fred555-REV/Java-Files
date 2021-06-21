5f184677613676250c73d80c225df48d7b9d17e4
Freddy Canido <54070099+Fred555-REV@users.noreply.github.com>
2021-06-21 18:38:41 -0400

    Add files via upload

---
+++ felicias-fruit-stand-3/D061921/Display.java
@@ -0,0 +1,21 @@
+package D061921;
+
+import java.util.List;
+
+public class Display {
+    public static void displayProducts(List productList, String type, int fruitA, int meatA) {
+
+        switch (type) {
+            case "meat":
+                for (int i = 0; i < productList.size() - fruitA; i++) {
+                    System.out.println(productList.get(i));
+                }
+                break;
+            case "fruit":
+                for (int i = meatA; i < productList.size(); i++) {
+                    System.out.println(productList.get(i));
+                }
+                break;
+        }
+    }
+}
